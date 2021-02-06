64edd5739adb5cd5559065868a8851645753c93c
Kiana Long <kianajade42.kl@gmail.com>
2021-02-06 17:59:45 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,11 @@
 class Project
-  attr_reader :title, :backers
-
+  attr_reader :title, :backed_projects
   def initialize(title)
     @title = title
-    @backers = []
+    @backed_projects = []
   end
-
   def add_backer(backer)
     backers << backer
     backer.backed_projects << self
   end
-
 end
